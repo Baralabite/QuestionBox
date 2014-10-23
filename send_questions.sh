7690575d20329bf63cc8d6a27deb07e29ec448d9
John Board <johnrobboard@gmail.com>
2014-10-23 04:25:10 -0600

    Updated send_questions.sh

--- send_questions.sh
+++ send_questions.sh
@@ -1,1 +1,11 @@
-echo "Questions for this week" | mail -s Questions -a questions.txt johnrobboard@gmail.com
+recipients="johnrobboard@gmail.com,johnsBogAcc@gmail.com"
+textdoc="/home3/springq0/public_html/johnrobboard/QuestionBox/questions.txt"
+
+echo "Questions from the 24/7 Youth Group's virtual question box." | mail -s Questions -a $textdoc $recipients
+
+if [ "$?" = "1" ]; then
+	echo "No questions this week from 24/7 Youth Group's virtual question box." | mail -s Questions $recipients
+else
+	rm $textdoc
+fi
+
